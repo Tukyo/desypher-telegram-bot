36904abf01ac351e9ec61504bd6ef47c312c1da6
TUKYO <82132843+Tukyo@users.noreply.github.com>
2024-05-15 23:44:53 -0700

    Rate Limiter Implemented - Token Price Fetching in Local Currencies

--- bot.py
+++ bot.py
@@ -130,24 +130,35 @@ class AntiRaid:
 anti_spam = AntiSpam(rate_limit=5, time_window=10, mute_time=60)
 anti_raid = AntiRaid(user_amount=20, time_out=30, anti_raid_time=180)
 
+RATE_LIMIT = 100  # Maximum number of allowed commands
+TIME_PERIOD = 60  # Time period in seconds
+last_check_time = time.time()
+command_count = 0
+
 user_verification_progress = {}
 
 #region Main Slash Commands
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello! I am the deSypher Bot. For a list of commands, please use /help.')
+    if rate_limit_check():
+        update.message.reply_text('Hello! I am the deSypher Bot. For a list of commands, please use /help.')
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def help(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        'Here are the commands you can use:\n'
-        '\n'
-        '/start: This starts the bot, and sends a welcome message.\n'
-        '/help: This is where you are now, you can see a list of commands here.\n'
-        '/play: Start a mini-game of deSypher within Telegram. Have fun!\n'
-        '/tukyo: This will provide information about the developer of this bot, and deSypher.\n'
-        '/tukyogames: This will provide information about Tukyo Games and our projects.\n'
-        '/deSypher: This will direct you to the main game, you can play it using SYPHER tokens!\n'
-        '/sypher: This command will provide you with information about the SYPHER token.\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            'Here are the commands you can use:\n'
+            '\n'
+            '/start: This starts the bot, and sends a welcome message.\n'
+            '/help: This is where you are now, you can see a list of commands here.\n'
+            '/play: Start a mini-game of deSypher within Telegram. Have fun!\n'
+            '/tukyo: This will provide information about the developer of this bot, and deSypher.\n'
+            '/tukyogames: This will provide information about Tukyo Games and our projects.\n'
+            '/deSypher: This will direct you to the main game, you can play it using SYPHER tokens!\n'
+            '/sypher: This command will provide you with information about the SYPHER token.\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 #region Play Game
 def play(update: Update, context: CallbackContext) -> None:
@@ -322,68 +333,176 @@ def fetch_random_word() -> str:
 #endregion Play Game
 
 def tukyo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        'Tukyo is the developer of this bot, deSypher and other projects. There are many impersonators, the only real Tukyo on telegram is @tukyowave.\n'
-        '\n'
-        '| Socials |\n'
-        'Website: https://www.tukyo.org/\n'
-        'Twitter/X: https://twitter.com/TUKYOWAVE\n'
-        'Instagram: https://www.instagram.com/tukyowave/\n'
-        'Medium: https://tukyo.medium.com/\n'
-        'Spotify: https://sptfy.com/QGbt\n'
-        'Bandcamp: https://tukyo.bandcamp.com/\n'
-        'Github: https://github.com/tukyo\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            'Tukyo is the developer of this bot, deSypher and other projects. There are many impersonators, the only real Tukyo on telegram is @tukyowave.\n'
+            '\n'
+            '| Socials |\n'
+            'Website: https://www.tukyo.org/\n'
+            'Twitter/X: https://twitter.com/TUKYOWAVE\n'
+            'Instagram: https://www.instagram.com/tukyowave/\n'
+            'Medium: https://tukyo.medium.com/\n'
+            'Spotify: https://sptfy.com/QGbt\n'
+            'Bandcamp: https://tukyo.bandcamp.com/\n'
+            'Github: https://github.com/tukyo\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def tukyogames(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        'Tukyo Games is a game development studio that is focused on bringing innovative blockchain technology to captivating and new game ideas. We use blockchain technology, without hindering the gaming experience.\n'
-        '\n'
-        'Website: https://tukyogames.com/ (Coming Soon)\n'
-        '\n'
-        '| Projects |\n'
-        'deSypher: https://desypher.net/\n'
-        'Super G.I.M.P. Girl: https://superhobogimpgirl.com/\n'
-        'Profectio: https://www.tukyowave.com/projects/profectio\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            'Tukyo Games is a game development studio that is focused on bringing innovative blockchain technology to captivating and new game ideas. We use blockchain technology, without hindering the gaming experience.\n'
+            '\n'
+            'Website: https://tukyogames.com/ (Coming Soon)\n'
+            '\n'
+            '| Projects |\n'
+            'deSypher: https://desypher.net/\n'
+            'Super G.I.M.P. Girl: https://superhobogimpgirl.com/\n'
+            'Profectio: https://www.tukyowave.com/projects/profectio\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def deSypher(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        'deSypher is an Onchain puzzle game that can be played on Base. It is a game that requires SYPHER to play. The goal of the game is to guess the correct word in four attempts. Guess the correct word, or go broke!\n'
-        '\n'
-        'Website: https://desypher.net/\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            'deSypher is an Onchain puzzle game that can be played on Base. It is a game that requires SYPHER to play. The goal of the game is to guess the correct word in four attempts. Guess the correct word, or go broke!\n'
+            '\n'
+            'Website: https://desypher.net/\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def sypher(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        'SYPHER is the native token of deSypher. It is used to play the game, and can be earned by playing the game.\n'
-        '\n'
-        'Get SYPHER: [Uniswap](https://app.uniswap.org/#/swap?outputCurrency=0x21b9D428EB20FA075A29d51813E57BAb85406620)\n'
-        'BaseScan: [Link](https://basescan.org/token/0x21b9d428eb20fa075a29d51813e57bab85406620)\n'
-        'Contract Address: 0x21b9D428EB20FA075A29d51813E57BAb85406620\n'
-        'Total Supply: 1,000,000\n'
-        'Blockchain: Base\n'
-        'Liquidity: Uniswap\n'
-        'Ticker: SYPHER\n',
-        parse_mode='Markdown',
-        disable_web_page_preview=True
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            'SYPHER is the native token of deSypher. It is used to play the game, and can be earned by playing the game.\n'
+            '\n'
+            'Get SYPHER: [Uniswap](https://app.uniswap.org/#/swap?outputCurrency=0x21b9D428EB20FA075A29d51813E57BAb85406620)\n'
+            'BaseScan: [Link](https://basescan.org/token/0x21b9d428eb20fa075a29d51813e57bab85406620)\n'
+            'Contract Address: 0x21b9D428EB20FA075A29d51813E57BAb85406620\n'
+            'Total Supply: 1,000,000\n'
+            'Blockchain: Base\n'
+            'Liquidity: Uniswap\n'
+            'Ticker: SYPHER\n',
+            parse_mode='Markdown',
+            disable_web_page_preview=True
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def ca(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        '0x21b9D428EB20FA075A29d51813E57BAb85406620\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            '0x21b9D428EB20FA075A29d51813E57BAb85406620\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def whitepaper(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-    'Whitepaper: https://desypher.net/whitepaper.html\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+        'Whitepaper: https://desypher.net/whitepaper.html\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 
 def website(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        'https://desypher.net/\n'
-    )
+    if rate_limit_check():
+        update.message.reply_text(
+            'https://desypher.net/\n'
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
 #endregion Main Slash Commands
+
+#region Ethereum Logic
+def get_token_price_in_weth(contract_address):
+    apiUrl = f"https://api.dexscreener.com/latest/dex/tokens/{contract_address}"
+    try:
+        response = requests.get(apiUrl)
+        response.raise_for_status()
+        data = response.json()
+        
+        if data['pairs'] and len(data['pairs']) > 0:
+            # Find the pair with WETH as the quote token
+            weth_pair = next((pair for pair in data['pairs'] if pair['quoteToken']['symbol'] == 'WETH'), None)
+            
+            if weth_pair:
+                price_in_weth = weth_pair['priceNative']
+                print(f"Price of the token in WETH: {price_in_weth}")
+                return price_in_weth
+            else:
+                print("No WETH pair found for this token.")
+                return None
+        else:
+            print("No pairs found for this token.")
+            return None
+    except requests.RequestException as e:
+        print(f"Error fetching token price from DexScreener: {e}")
+        return None
+    
+def get_weth_price_in_fiat(currency):
+    apiUrl = f"https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies={currency}"
+    try:
+        response = requests.get(apiUrl)
+        response.raise_for_status()  # This will raise an exception for HTTP errors
+        data = response.json()
+        return data['ethereum'][currency]
+    except requests.RequestException as e:
+        print(f"Error fetching WETH price from CoinGecko: {e}")
+        return None
+    
+def get_token_price_in_fiat(contract_address, currency):
+    # Fetch price of token in WETH
+    token_price_in_weth = get_token_price_in_weth(contract_address)
+    if token_price_in_weth is None:
+        print("Could not retrieve token price in WETH.")
+        return None
+
+    # Fetch price of WETH in the specified currency
+    weth_price_in_fiat = get_weth_price_in_fiat(currency)
+    if weth_price_in_fiat is None:
+        print(f"Could not retrieve WETH price in {currency}.")
+        return None
+
+    # Calculate token price in the specified currency
+    token_price_in_fiat = float(token_price_in_weth) * weth_price_in_fiat
+    return token_price_in_fiat
+#endregion Ethereum Logic
+
+#region Ethereum Slash Commands
+def price(update: Update, context: CallbackContext) -> None:
+    if rate_limit_check():
+        currency = context.args[0] if context.args else 'usd'
+        currency = currency.lower()
+
+        # Check if the provided currency is supported
+        if currency not in ['usd', 'eur']:
+            update.message.reply_text("Unsupported currency. Please use 'usd' or 'eur'.")
+            return
+
+        # Fetch and format the token price in the specified currency
+        token_price_in_fiat = get_token_price_in_fiat(contract_address, currency)
+        if token_price_in_fiat is not None:
+            formatted_price = format(token_price_in_fiat, '.4f')
+            update.message.reply_text(f"SYPHER: {currency.upper()}: {formatted_price}")
+        else:
+            update.message.reply_text(f"Failed to retrieve the price of the token in {currency.upper()}.")
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
+
+def chart(update: Update, context: CallbackContext) -> None:
+    if rate_limit_check():
+        update.message.reply_text(
+            '[Dexscreener](https://dexscreener.com/base/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b) • [Dextools](https://www.dextools.io/app/en/base/pair-explorer/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b?t=1715831623074) • [CMC](https://coinmarketcap.com/dexscan/base/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b/) • [CG](https://www.geckoterminal.com/base/pools/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b?utm_source=coingecko)\n',
+            parse_mode='Markdown',
+            disable_web_page_preview=True
+        )
+    else:
+        update.message.reply_text('Bot rate limit exceeded. Please try again later.')
+#endregion Ethereum Slash Commands
 
 #region User Verification
 def handle_new_user(update: Update, context: CallbackContext) -> None:
@@ -568,77 +687,6 @@ def button_callback(update: Update, context: CallbackContext) -> None:
     query.edit_message_text(text="A verification message has been sent to your DMs. Please check your messages.")
 #endregion User Verification
 
-#region Ethereum Logic
-def get_token_price_in_weth(contract_address):
-    apiUrl = f"https://api.dexscreener.com/latest/dex/tokens/{contract_address}"
-    try:
-        response = requests.get(apiUrl)
-        response.raise_for_status()
-        data = response.json()
-        
-        if data['pairs'] and len(data['pairs']) > 0:
-            # Find the pair with WETH as the quote token
-            weth_pair = next((pair for pair in data['pairs'] if pair['quoteToken']['symbol'] == 'WETH'), None)
-            
-            if weth_pair:
-                price_in_weth = weth_pair['priceNative']
-                print(f"Price of the token in WETH: {price_in_weth}")
-                return price_in_weth
-            else:
-                print("No WETH pair found for this token.")
-                return None
-        else:
-            print("No pairs found for this token.")
-            return None
-    except requests.RequestException as e:
-        print(f"Error fetching token price from DexScreener: {e}")
-        return None
-    
-def get_weth_price_in_usd():
-    apiUrl = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
-    try:
-        response = requests.get(apiUrl)
-        response.raise_for_status()  # This will raise an exception for HTTP errors
-        data = response.json()
-        return data['ethereum']['usd']
-    except requests.RequestException as e:
-        print(f"Error fetching WETH price from CoinGecko: {e}")
-        return None
-    
-def get_token_price_in_usd(contract_address):
-    # Fetch price of token in WETH
-    token_price_in_weth = get_token_price_in_weth(contract_address)
-    if token_price_in_weth is None:
-        print("Could not retrieve token price in WETH.")
-        return None
-
-    # Fetch price of WETH in USD
-    weth_price_in_usd = get_weth_price_in_usd()
-    if weth_price_in_usd is None:
-        print("Could not retrieve WETH price in USD.")
-        return None
-
-    # Calculate token price in USD
-    token_price_in_usd = float(token_price_in_weth) * weth_price_in_usd
-    return token_price_in_usd
-#endregion Ethereum Logic
-
-#region Ethereum Slash Commands
-def price(update: Update, context: CallbackContext) -> None:
-    token_price_in_usd = get_token_price_in_usd(contract_address)
-    if token_price_in_usd is not None:
-        update.message.reply_text(f"SYPHER: ${token_price_in_usd}")
-    else:
-        update.message.reply_text("Failed to retrieve the price of the token in USD.")
-
-def chart(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(
-        '[Dexscreener](https://dexscreener.com/base/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b) - [Dextools](https://www.dextools.io/app/en/base/pair-explorer/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b?t=1715831623074) - [CoinMarketCap](https://coinmarketcap.com/dexscan/base/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b/) - [CoinGecko](https://www.geckoterminal.com/base/pools/0xb0fbaa5c7d28b33ac18d9861d4909396c1b8029b?utm_source=coingecko)\n',
-        parse_mode='Markdown',
-        disable_web_page_preview=True
-    )
-#endregion Ethereum Slash Commands
-
 #region Admin Controls
 def unmute_user(context: CallbackContext) -> None:
     job = context.job
@@ -669,6 +717,22 @@ def handle_message(update: Update, context: CallbackContext) -> None:
         # Schedule job to unmute the user
         job_queue = context.job_queue
         job_queue.run_once(unmute_user, mute_time, context={'chat_id': chat_id, 'user_id': user_id})
+
+def rate_limit_check():
+    global last_check_time, command_count
+    current_time = time.time()
+
+    # Reset count if time period has expired
+    if current_time - last_check_time > TIME_PERIOD:
+        command_count = 0
+        last_check_time = current_time
+
+    # Check if the bot is within the rate limit
+    if command_count < RATE_LIMIT:
+        command_count += 1
+        return True
+    else:
+        return False
 #endregion Admin Controls
 
 #region Admin Slash Commands
